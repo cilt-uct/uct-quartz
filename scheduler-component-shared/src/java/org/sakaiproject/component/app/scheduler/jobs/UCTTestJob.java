0f2c610d715cc9c203f6062e168834fe2ce49145
David Horwitz <dhorwitz@gmail.com>
2008-03-17 08:47:17 +0000

    add some logging

--- scheduler-component-shared/src/java/org/sakaiproject/component/app/scheduler/jobs/UCTTestJob.java
+++ scheduler-component-shared/src/java/org/sakaiproject/component/app/scheduler/jobs/UCTTestJob.java
@@ -1,21 +1,24 @@
 package org.sakaiproject.component.app.scheduler.jobs;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
 import org.quartz.StatefulJob;
 
 public class UCTTestJob implements StatefulJob {
-
+	private static final Log LOG = LogFactory.getLog(UCTTestJob.class);
+	
 	public void execute(JobExecutionContext arg0) throws JobExecutionException {
 		// TODO Auto-generated method stub
-
+		LOG.info("UCTTestJob fired");
 		try {
 			Thread.sleep(120*1000);
 		} catch (InterruptedException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+		LOG.info("UCTTestJob finnished");
 	}
 
 }
