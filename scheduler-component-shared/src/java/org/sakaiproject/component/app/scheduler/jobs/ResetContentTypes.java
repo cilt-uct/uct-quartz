511366d973c6f66a26c9fc57bfa6421c53c85501
David Horwitz <dhorwitz@gmail.com>
2009-01-13 15:21:26 +0000

    VULA-263 basics in place still need to inject the extension/mime map rather than hard coding it

---
+++ scheduler-component-shared/src/java/org/sakaiproject/component/app/scheduler/jobs/ResetContentTypes.java
@@ -0,0 +1,131 @@
+package org.sakaiproject.component.app.scheduler.jobs;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.quartz.Job;
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
+import org.sakaiproject.content.api.ContentCollection;
+import org.sakaiproject.content.api.ContentHostingService;
+import org.sakaiproject.content.api.ContentResource;
+import org.sakaiproject.content.api.ContentResourceEdit;
+import org.sakaiproject.exception.IdUnusedException;
+import org.sakaiproject.exception.InUseException;
+import org.sakaiproject.exception.OverQuotaException;
+import org.sakaiproject.exception.PermissionException;
+import org.sakaiproject.exception.ServerOverloadException;
+import org.sakaiproject.exception.TypeException;
+import org.sakaiproject.site.api.Site;
+import org.sakaiproject.site.api.SiteService;
+import org.sakaiproject.site.api.SiteService.SortType;
+import org.sakaiproject.tool.api.Session;
+import org.sakaiproject.tool.api.SessionManager;
+
+
+
+
+public class ResetContentTypes implements Job {
+
+	private SiteService siteService;
+	public void setSiteService(SiteService s) {
+		this.siteService = s;
+	}
+	
+	private SessionManager sessionManager;
+	public void setSessionManager(SessionManager s) {
+		this.sessionManager = s;
+	}
+	
+	Map<String, String> extensions = new HashMap<String, String>();
+	
+	public void setExtensions(Map<String, String> extensions) {
+		this.extensions = extensions;
+	}
+
+	private ContentHostingService contentHostingService;
+	public void setContentHostingService(ContentHostingService chs) {
+		contentHostingService = chs;
+	}
+	
+	
+	private static final Log LOG = LogFactory.getLog(ResetContentTypes.class);
+	
+	public void execute(JobExecutionContext arg0) throws JobExecutionException {
+		//set the user information into the current session
+		//for now 
+		extensions.put("doc", "application/msword");
+		extensions.put("odt", "application/openoffice");
+		
+	    Session sakaiSession = sessionManager.getCurrentSession();
+	    sakaiSession.setUserId("admin");
+	    sakaiSession.setUserEid("admin");
+	    List sites = siteService.getSites(SiteService.SelectionType.ANY, null , null, null, SortType.NONE, null);
+	    for (int i =0 ; i< sites.size(); i++ ) {
+	    	Site s = (Site)sites.get(i);
+	    	String siteColl = contentHostingService.getSiteCollection(s.getId());
+	    	ContentCollection collection;
+			try {
+				collection = contentHostingService.getCollection(siteColl);
+		    	List members = collection.getMembers();
+		    	for (int q =0; q < members.size(); q++) {
+		    		String resId = (String)members.get(q);
+		    		LOG.info("got resource " + resId);
+		    		if (reset(resId)) {
+		    			ContentResourceEdit res = contentHostingService.editResource(resId);
+		    			LOG.info("content had type: " + res.getContentType());
+		    			res.setContentType(getContentType(resId));
+		    			contentHostingService.commitResource(res, 0);
+		    		}
+		    	}
+			} catch (IdUnusedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (TypeException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (PermissionException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (InUseException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (OverQuotaException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (ServerOverloadException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
+	    }
+
+	}
+
+	private String getContentType(String resId) {
+		return extensions.get(getExtension(resId));
+	}
+
+	
+	private boolean reset(String resId) {
+		String extension = getExtension(resId);
+		if (extensions.containsKey(extension)) {
+			return true;
+		}
+		return false;
+	}
+
+	private String getExtension(String resId) {
+		String extension = null;
+		if (resId.indexOf(".") > 0 ) {
+			extension = resId.substring(resId.lastIndexOf(".") + 1, resId.length());
+			LOG.info("got extension: " + extension);
+		}
+			
+		return extension;
+	}
+
+}
