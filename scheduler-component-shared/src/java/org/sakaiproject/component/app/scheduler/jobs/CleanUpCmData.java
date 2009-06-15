7f20e69d7fce1ed218f326e0bfbd44fd4b93e98f
David Horwitz <dhorwitz@gmail.com>
2009-06-15 12:42:33 +0000

    NOJIRA add generics

--- scheduler-component-shared/src/java/org/sakaiproject/component/app/scheduler/jobs/CleanUpCmData.java
+++ scheduler-component-shared/src/java/org/sakaiproject/component/app/scheduler/jobs/CleanUpCmData.java
@@ -6,11 +6,9 @@ import java.util.Set;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.hibernate.ObjectNotFoundException;
 import org.quartz.Job;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
-import org.sakaiproject.coursemanagement.api.AcademicSession;
 import org.sakaiproject.coursemanagement.api.CanonicalCourse;
 import org.sakaiproject.coursemanagement.api.CourseManagementAdministration;
 import org.sakaiproject.coursemanagement.api.CourseManagementService;
@@ -53,16 +51,16 @@ public class CleanUpCmData implements Job {
 	    sakaiSession.setUserId(ADMIN);
 	    sakaiSession.setUserEid(ADMIN);
 	    
-	    AcademicSession thisTerm = courseManagementService.getAcademicSession(term);
+	    //AcademicSession thisTerm = courseManagementService.getAcademicSession(term);
 	    
 	    //first get course sets
-	    List depts = courseManagementService.findCourseSets("Department");
+	    List<CourseSet> depts = courseManagementService.findCourseSets("Department");
 	    
 	    for (int i =0 ; i < depts.size(); i++) {
 	    	CourseSet dept = (CourseSet) depts.get(i);
 	    	log.info("got set: " + dept.getEid());
-	    	Set canonCourses = courseManagementService.getCanonicalCourses(dept.getEid());
-	    	Iterator it = canonCourses.iterator();
+	    	Set<CanonicalCourse> canonCourses = courseManagementService.getCanonicalCourses(dept.getEid());
+	    	Iterator<CanonicalCourse> it = canonCourses.iterator();
 	    	while (it.hasNext()) {
 	    		CanonicalCourse course = (CanonicalCourse) it.next();
 	    		String eid = course.getEid();
@@ -71,8 +69,8 @@ public class CleanUpCmData implements Job {
 	    			String fullEid = eid + "," + term;
 	    			//we only need to remove at this level
 	    			//we need to remove the course offering from its course set
-	    			Set courseSets = course.getCourseSetEids();
-	    			Iterator ita = courseSets.iterator();
+	    			Set<String> courseSets = course.getCourseSetEids();
+	    			Iterator<String> ita = courseSets.iterator();
 	    			while (ita.hasNext()) {
 	    				String set = (String)ita.next();
 	    				courseAdmin.removeCanonicalCourseFromCourseSet(set, eid);
@@ -81,8 +79,8 @@ public class CleanUpCmData implements Job {
 	    			//course offering
 	    			if (courseManagementService.isCourseOfferingDefined(fullEid)) {
 	    				CourseOffering co = courseManagementService.getCourseOffering(fullEid);
-	    				Set sets = co.getCourseSetEids();
-	    				Iterator itb = sets.iterator();
+	    				Set<String> sets = co.getCourseSetEids();
+	    				Iterator<String> itb = sets.iterator();
 	    				while (itb.hasNext()) {
 	    					String set = (String)itb.next();
 	    					courseAdmin.removeCourseOfferingFromCourseSet(set, fullEid);
