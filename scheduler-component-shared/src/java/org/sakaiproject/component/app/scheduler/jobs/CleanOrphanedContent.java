567869917acef5909830551df8731f13b2826742
David Horwitz <dhorwitz@gmail.com>
2010-02-17 13:21:06 +0000

    VULA-858 clean up logging

--- scheduler-component-shared/src/java/org/sakaiproject/component/app/scheduler/jobs/CleanOrphanedContent.java
+++ scheduler-component-shared/src/java/org/sakaiproject/component/app/scheduler/jobs/CleanOrphanedContent.java
@@ -46,27 +46,29 @@ public class CleanOrphanedContent implements Job {
 		String sql = "select collection_id as cuid from CONTENT_COLLECTION where in_collection='/user/' and (replace(mid(collection_id,7),'/','') not in (SELECT user_id from SAKAI_USER_ID_MAP));";
 		List<String> res = sqlService.dbRead(sql);
 		long userBytes = getBytesInCollection(res);
-		log.info("Orpahned content in user collections: " + userBytes);
+		
 		
 		sql = "select collection_id from CONTENT_COLLECTION where in_collection='/group/' and replace(mid(collection_id,7),'/','') not in (select site_id from SAKAI_SITE); ";
 		res = sqlService.dbRead(sql);
 		long siteBytes = getBytesInCollection(res);
-		log.info("Orpahned content in site collections: " + siteBytes);
+		
 		
 		sql = "select collection_id from CONTENT_COLLECTION where in_collection='/attachment/' and replace(mid(collection_id,13),'/','') not in (select site_id from SAKAI_SITE); ";
 		res = sqlService.dbRead(sql);
 		long attachBytes = getBytesInCollection(res);
+		
+		log.info("Orpahned content in user collections: " + userBytes);
 		log.info("Orpahned content in attachment collections: " + attachBytes);
-	    
+		log.info("Orpahned content in site collections: " + siteBytes);
 	}
 	
 	
-	private long getBytesInCollection(List collectionList) {
+	private long getBytesInCollection(List<String> collectionList) {
 		long userBytes = 0;
 		log.info("got a result of: " + collectionList.size());
 		for (int i =0 ; i < collectionList.size(); i ++) {
 			String r = (String)collectionList.get(i);
-			log.info("got resource: " + r);
+			log.debug("got resource: " + r);
 			
 			try {
 				ContentCollection  collection = contentHostingService.getCollection(r);
