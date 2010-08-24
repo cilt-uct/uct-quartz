d846bb46b613bc278483f4c1e38728acf5b228e9
David Horwitz <dhorwitz@gmail.com>
2010-08-24 08:41:57 +0000

    NOJIRA remove unused field

--- scheduler-component-shared/src/java/org/sakaiproject/component/app/scheduler/jobs/UCTImportCourses.java
+++ scheduler-component-shared/src/java/org/sakaiproject/component/app/scheduler/jobs/UCTImportCourses.java
@@ -4,8 +4,6 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
 
