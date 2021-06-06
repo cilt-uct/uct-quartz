8d5a42aa216946477c5bf42a7b885480db70bf51
Stephen Marquard <stephen.marquard@uct.ac.za>
2021-06-06 19:09:03 +0200

    Check for null threads in the thread dumper

--- src/java/za/ac/uct/sakai/healthcheck/ServerHealthCheck.java
+++ src/java/za/ac/uct/sakai/healthcheck/ServerHealthCheck.java
@@ -241,18 +241,22 @@ public class ServerHealthCheck  {
         final ThreadMXBean threadMXBean = ManagementFactory.getThreadMXBean();
         final ThreadInfo[] threadInfos = threadMXBean.getThreadInfo(threadMXBean.getAllThreadIds(), 100);
         for (ThreadInfo threadInfo : threadInfos) {
-            dump.append('"');
-            dump.append(threadInfo.getThreadName());
-            dump.append("\" ");
-            final Thread.State state = threadInfo.getThreadState();
-            dump.append("\n   java.lang.Thread.State: ");
-            dump.append(state);
-            final StackTraceElement[] stackTraceElements = threadInfo.getStackTrace();
-            for (final StackTraceElement stackTraceElement : stackTraceElements) {
-                dump.append("\n        at ");
-                dump.append(stackTraceElement);
-            }
-            dump.append("\n\n");
+            if (threadInfo != null) {
+		    dump.append('"');
+		    dump.append(threadInfo.getThreadName());
+		    dump.append("\" ");
+		    final Thread.State state = threadInfo.getThreadState();
+		    dump.append("\n   java.lang.Thread.State: ");
+		    dump.append(state);
+		    final StackTraceElement[] stackTraceElements = threadInfo.getStackTrace();
+		    for (final StackTraceElement stackTraceElement : stackTraceElements) {
+			if (stackTraceElement != null) {
+				dump.append("\n        at ");
+				dump.append(stackTraceElement);
+			}
+		    }
+		    dump.append("\n\n");
+	    }
         }
         return dump.toString();
     }
